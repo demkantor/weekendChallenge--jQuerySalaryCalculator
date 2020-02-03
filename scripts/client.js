6f8b800f98ac9bee87bd13d51348d22f4ef6c8e4
demkantor <demkantor@gmail.com>
2020-02-02 21:27:22 -0600

    added styling

--- scripts/client.js
+++ scripts/client.js
@@ -1,4 +1,3 @@
-console.log("Mellow like jellelo")
 
 //globals
 let employeeSalaryList = [0];
@@ -7,12 +6,34 @@ let employeeSalaryList = [0];
 $(document).ready(onReady);
 
 function onReady(){
-    console.log("cool like lemonade");
     $('#submitButton').on('click', displayEmployee);
     $("#tableBody").on('click', ".employee", ".deleteButton", deleteEmployee);
 }
+
+function deleteEmployee(){
+    deleteEmployeeSalary();
+    let el = $(this);
+    el.remove();
+}//end delete employee
+
+function deleteEmployeeSalary(){
+    let el = $(this).parent();
+    console.log(el);
+    let moneyName = document.getElementsByClassName('.employee');
+    console.log(moneyName);
+    let molla = document.getElementById('tableBody').getElementsByClassName('money');
+    console.log(molla);
+}//remove salary of deleted employee from total
+
 function displayEmployee(){
-    console.log('in displayEmployee');
+    //be sure all fields are filled in
+    if (document.getElementById("firstNameIn").value === ""
+     || document.getElementById("lastNameIn").value === ""
+     || document.getElementById("idIn").value === ""
+     || document.getElementById("titleIn").value === ""
+     || document.getElementById("salaryIn").value === ""){
+       alert("Please be sure to fill in all fields");
+   } else {
     // sending employee object to new table row
     let el = $('#tableBody');
     let firstName = $('#firstNameIn').val();
@@ -21,66 +42,25 @@ function displayEmployee(){
     let title = $('#titleIn').val();
     let anualSalary = $('#salaryIn').val();
         el.append(`<tr class=employee><td>${firstName}</td><td>${lastName}</td>
-        <td>${id}</td><td>${title}</td><td>${anualSalary}</td>
+        <td>${id}</td><td>${title}</td><td class="money">${anualSalary}</td>
         <td><button class="deleteButton">X</button></td></tr>`);
     //add anual salary to array
     employeeSalaryList.push(anualSalary);
-    console.log(employeeSalaryList);
     //show monthly salay in DOM
     displayMonthlySalary();
+   }
 }//end displayEmployee
 
-function deleteEmployee(){
-    let el = $(this);
-    console.log(el);
-    el.remove();
-}//end delete employee
-
-
 function displayMonthlySalary(){
-    console.log("Hold it now...");
     let changeToNumber = employeeSalaryList.map(Number);
     let aunalSum = changeToNumber.reduce((a, b) => a + b, 0);
-    console.log(aunalSum);
     let monthlySum = aunalSum / 12
-    console.log(monthlySum);
+    let fixedMonthlySum = (Math.round(monthlySum * 100) / 100).toFixed(2);
     let el = $('.monthlySalary');
-    el.text(`${monthlySum}`);
+    el.text(`${fixedMonthlySum}`);
+    if (monthlySum > 20000){
+        $('.monthlySalary').css('color', 'red');
+    }
 }//end displayMonthlySalary
 
 
-
-
-
-
-
-
-
-// function addEmployee(){
-//     console.log('in addEmployee');
-//     //get user input & create a new employee object
-//     let newEmployee = {
-//         firstName: $('#firstNameIn').val(),
-//         lastName: $('#lastNameIn').val(),
-//         id: $('#idIn').val(),
-//         title: $('#titleIn').val(),
-//         anualSalary: $('#salaryIn').val()
-//     } // end newEmployee
-//     console.log('adding:', newEmployee);
-//     employeeList.push(newEmployee);
-//     displayEmployee();
-//     //monthlySalary();
-// }//end addEmployee
-
-// function displayEmployee(){
-//     console.log('in displayEmployee');
-//     // sending employee object to new table row
-//     let el = $('#tableBody');
-//     // loop through array
-//     for(let i=0; i<employeeList.length; i++ ){
-//         // append each item to DOM
-//         el.append(`<tr class=employee><td>${employeeList[i].firstName}</td><td>${employeeList[i].lastName}</td>
-//         <td>${employeeList[i].id}</td><td>${employeeList[i].title}</td>
-//         <td>${employeeList[i].anualSalary}</td><td><button class="deleteButton">X</button></td></tr>`);
-//     }
-// }//end displayEmployee
